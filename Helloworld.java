eb941f06e444ffbdc6d2150d82981ca5939b9922
Harshit Dubey <hdlikes8@gmail.com>
2022-02-08 14:53:29 +0530

    first commit

---
+++ Helloworld.java
@@ -0,0 +1,5 @@
+class Hello {
+    public static void main(String args[]) {
+        System.out.println("Hello world my name is Harshit and today is 8/2/22");
+    }
+}
---
+++ OOP/objects.java
@@ -0,0 +1,24 @@
+package OOP;
+
+class Pen {
+
+    String color;
+    String type;
+
+    public void write() {
+        System.out.println("wrrit");
+    }
+
+}
+
+public class objects {
+
+    public static void main(String args[]) {
+        Pen pen1 = new Pen();
+        pen1.color = "blue";
+        pen1.type = "gel";
+
+        pen1.write();
+
+    }
+}
